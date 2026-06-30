fd2c5a48992596d31dce9c7cffabf2cca2e16573
Md Mostafizur Rahman <mustafizurr6490@gmail.com>
2026-06-30 06:38:22 +0600

    Delete Technical Solutions of Module 1: Payroll Compliance & Budgetary Controls /The Multi-Metric Departmental Variance Report/The Departmental Distance Audit (Window Functions).sql

--- Technical Solutions of Module 1: Payroll Compliance & Budgetary Controls /The Multi-Metric Departmental Variance Report/The Departmental Distance Audit (Window Functions).sql
+++
@@ -1,8 +0,0 @@
-
-SELECT 
-    first_name,
-    department_id,
-    salary,
-    AVG(salary) OVER(PARTITION BY department_id) AS [Dept_Avg],
-    salary - AVG(salary) OVER(PARTITION BY department_id) AS [Dept_Distance]
-FROM employees;
